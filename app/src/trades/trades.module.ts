abc6a0051bea1c0262c2ae4826bf43cbc42062f8
EliteOneTube <nickhaskaris@gmail.com>
2025-08-26 15:57:41 +0300

    fix: Missing import of HttpModule

--- app/src/trades/trades.module.ts
+++ app/src/trades/trades.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TradesRabbitService } from './trades-rabbit.service';
 import { TradesService } from './trades.service';
 import { RabbitMQWrapperModule } from 'src/rabbitmq-wrapper/rabbitmq-wrapper.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [RabbitMQWrapperModule],
+  imports: [RabbitMQWrapperModule, HttpModule],
   providers: [TradesRabbitService, TradesService],
 })
 export class TradesModule {}
